ce91581e53dcbb335697e8ca8ff270531dbdc307
Stuart Clark <sjclark76@gmail.com>
2020-06-24 14:32:25 +1200

    initialize project

---
+++ src/Marten.ApplicationInsights.DependencyTracker/Marten.ApplicationInsights.DependencyTracker/AppInsightsMartenLogger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using Marten.Services;
+using Microsoft.ApplicationInsights;
+using Npgsql;
+
+namespace Marten.ApplicationInsights.DependencyTracker
+{
+    public class AppInsightsMartenLogger : IMartenLogger, IMartenSessionLogger
+    {
+        private readonly TelemetryClient _telemetryClient;
+        private DateTime _startTime;
+        private Stopwatch _timer;
+
+        public AppInsightsMartenLogger(TelemetryClient telemetryClient)
+        {
+            _telemetryClient = telemetryClient;
+            _timer = new Stopwatch();
+        }
+
+        public IMartenSessionLogger StartSession(IQuerySession session)
+        {
+            _startTime = DateTime.UtcNow;
+            _timer = Stopwatch.StartNew();
+
+            return this;
+        }
+
+        public void SchemaChange(string sql)
+        {
+            // Do nothing.
+        }
+
+        public void LogSuccess(NpgsqlCommand command)
+        {
+            LogDependency(command, true);
+        }
+
+        public void LogFailure(NpgsqlCommand command, Exception ex)
+        {
+            LogDependency(command, false);
+        }
+
+        public void RecordSavedChanges(IDocumentSession session, IChangeSet commit)
+        {
+            // Do Nothing
+        }
+
+        private void LogDependency(NpgsqlCommand command, bool success)
+        {
+            _timer.Stop();
+
+            _telemetryClient.TrackDependency("PostgreSQL",
+                GetDependencyName(command),
+                command.CommandText,
+                _startTime,
+                _timer.Elapsed,
+                success);
+        }
+
+        private static string GetDependencyName(NpgsqlCommand command)
+        {
+            var dependencyName = command.Connection?.Host ?? string.Empty;
+
+            return dependencyName;
+        }
+    }
+}
